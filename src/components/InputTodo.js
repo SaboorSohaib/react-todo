f0b519f6697a04a7a59fb81b1f0bbd4f92a98ced
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-11-28 23:22:42 +0430

    Add some changes

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 
 class InputTodo extends Component {
-
     state = {
       title: '',
     };
-  
+
     onChange = (e) => {
       this.setState({
         [e.target.name]: e.target.value,
